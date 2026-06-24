6efbd02c5bdc0a81d5002d20669e29b9efb22817
siakinnik <siakin.nikita@siakinnik.com>
2026-06-24 23:04:57 +0200

    feat: add rich_message?: InputRichMessage; to editMessageCaption

--- methods.ts
+++ methods.ts
@@ -2014,6 +2014,7 @@ export type ApiMethods<F> = {
     link_preview_options?: LinkPreviewOptions;
     /** An object for an inline keyboard. */
     reply_markup?: InlineKeyboardMarkup;
+    rich_message?: InputRichMessage;
   }):
     & Update.Edited
     & (Message.TextMessage | Message.GameMessage)
@@ -2039,6 +2040,7 @@ export type ApiMethods<F> = {
     link_preview_options?: LinkPreviewOptions;
     /** An object for an inline keyboard. */
     reply_markup?: InlineKeyboardMarkup;
+    rich_message?: InputRichMessage;
   }): true;
 
   /** Use this method to edit captions of messages. On success, if the edited message is not an inline message, the edited Message is returned, otherwise True is returned. Note that business messages that were not sent by the bot and do not contain an inline keyboard can only be edited within 48 hours from the time they were sent. */
